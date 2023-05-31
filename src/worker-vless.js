e64fc21d98dd2c6ba6fe5b38c3b6ca923076223a
a4346422 <70024529+a4346422@users.noreply.github.com>
2023-05-31 14:11:24 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-const userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+const userID = '1ad51939-73cf-4f63-8e1d-cd8ab22fee5b';
 
 // 1. 如果这个你不填写，并且你客户端的 IP 不是 China IP，那么就自动取你的客户端IP。有一定概率会失败。
 // 2. 如果你指定，忽略一切条件，用你指定的IP。
